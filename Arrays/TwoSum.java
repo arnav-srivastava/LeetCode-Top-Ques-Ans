12ba9f33f04a01caeecfd1c587e8c8eb75f4676e
arnav-srivastava <arnavsrivastav398@gmail.com>
2025-08-30 08:52:05 +0530

    Tackling two sum appraoch using optimized soln ie. HashMap

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int x = target - nums[i];
+            if (hm.containsKey(x)) {
+                return new int[] { hm.get(x), i };
+            }
+            hm.put(nums[i], i);
+        }
+
+        return null;
+
+    }
+}
