4d58e566d7da703631a495bb6bb82216e3ff24b6
arnav-srivastava <arnavsrivastav398@gmail.com>
2025-09-01 08:47:46 +0530

    Takling Valid Anagram leetcode ques

---
+++ Arrays/ValidAnagram_242.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class ValidAnagram_242 {
+    public boolean isAnagram(String s, String t) {
+        int l1=s.length();
+        int l2=t.length();
+        if(l1 != l2) return false;
+
+        int[] count = new int[26];
+
+        for(int i=0;i<l1;i++){
+            count[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<l2;i++){
+            count[t.charAt(i)-'a']--;
+        }
+
+        for(int i=0;i<26;i++){
+            if(count[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
